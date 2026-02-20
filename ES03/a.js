b0480f0489564ad72f0fa887b10e0904f2c9fdae
Edoado Zaffaroni <edoardo.zaffaroni@gmail.com>
2026-02-20 09:32:01 +0000

    es a

---
+++ ES03/a.js
@@ -0,0 +1,6 @@
+const larghezza = prompt("Inserisci larghezza: ");
+const altezza = prompt("Inserisci altezza: ");
+const lunghezza = prompt("Inserisci lunghezza: ");
+
+const risultato = larghezza * altezza * lunghezza;
+console.log("Dimensione scatola: " + risultato);
