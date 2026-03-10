1bf20451215eed1fe9264b7384192a2cc968d0a2
edo2908 <edoardo.zaffaroni@gmail.com>
2026-03-10 18:56:01 +0100

    Update and rename a.js to a.html

--- ES03/a.js
+++
@@ -1,6 +0,0 @@
-const larghezza = prompt("Inserisci larghezza: ");
-const altezza = prompt("Inserisci altezza: ");
-const lunghezza = prompt("Inserisci lunghezza: ");
-
-const risultato = larghezza * altezza * lunghezza;
-console.log("Dimensione scatola: " + risultato);
