46e1479730ad44ee11aba1de6df2745753d79a5c
edo2908 <edoardo.zaffaroni@gmail.com>
2026-03-10 18:57:29 +0100

    Update c.js

--- ES03/c.js
+++ ES03/c.js
@@ -1,3 +1,16 @@
-const voto = 7;
-const risultato = voto >= 6 ? "Promosso" : "Bocciato";
-console.log(risultato);
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Esercizio C</title>
+  </head>
+  <body>
+    <h1>Esercizio C</h1>
+  </body>
+  <script>
+    const voto = 7;
+    const risultato = voto >= 6 ? "Promosso" : "Bocciato";
+    console.log(risultato);
+  </script>
+</html>
